622a976f774a600dabe597b0bcb6da59ee1f4369
Enzo Giacomelli <egiacomelli09@gmail.com>
2022-06-30 20:26:53 -0300

    opa

---
+++ app/src/main/java/com/HoW/calculadora3/MainActivity.java
@@ -0,0 +1,180 @@
+package com.HoW.calculadora3;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText resultadoText;
+    EditText operacaoText;
+    double num1 = 0;
+    double num2 = 0;
+    //String decimal1 = "";
+    //String decimal2 = "";
+    byte operador = 0;
+    Button adicaoBtn;
+    Button subtracaoBtn;
+    Button multiplicacaoBtn;
+    Button divisaoBtn;
+    Button resultadoBtn;
+    Button apagarBtn;
+    Button num0Btn;
+    Button num1Btn;
+    Button num2Btn;
+    Button num3Btn;
+    Button num4Btn;
+    Button num5Btn;
+    Button num6Btn;
+    Button num7Btn;
+    Button num8Btn;
+    Button num9Btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teste);
+
+        vincularComponentes();
+
+        operacoes Operacoes = new operacoes();
+    }
+
+    private void vincularComponentes()
+    {
+        resultadoText = findViewById(R.id.resultadoTxt);
+        operacaoText = findViewById(R.id.operacaoTxt);
+        adicaoBtn = findViewById(R.id.somaBtn);
+        subtracaoBtn = findViewById(R.id.subtracaoBtn);
+        multiplicacaoBtn = findViewById(R.id.multiplicacaoBtn);
+        divisaoBtn = findViewById(R.id.divisaoBtn);
+        resultadoBtn = findViewById(R.id.igualBtn);
+        apagarBtn = findViewById(R.id.apagarBtn);
+        num0Btn = findViewById(R.id.num0Btn);
+        num1Btn = findViewById(R.id.num1Btn);
+        num2Btn = findViewById(R.id.num2Btn);
+        num3Btn = findViewById(R.id.num3Btn);
+        num4Btn = findViewById(R.id.num4Btn);
+        num5Btn = findViewById(R.id.num5Btn);
+        num6Btn = findViewById(R.id.num6Btn);
+        num7Btn = findViewById(R.id.num7Btn);
+        num8Btn = findViewById(R.id.num8Btn);
+        num9Btn = findViewById(R.id.num9Btn);
+    }
+
+    public void separaNumeros(String operacao){
+        //operacao = "5 + 6";
+        String apenasNumeros;
+        apenasNumeros = operacao.replaceAll("[^0-9]", " ");
+        String partes[];
+        partes = apenasNumeros.split(" ");
+
+        num1 = Double.parseDouble(partes[0]);
+        //num2 = Double.parseDouble(partes[1]); //por algum motivo quebra o app quando atribiu o valor
+
+        resultadoText.setText(partes[0]);
+    }
+
+
+
+    public void clickLimparBtn(View view){
+        operacaoText.setText("0");
+    }
+
+    public void clickIgualBtn(View view){
+        separaNumeros(operacaoText.getText().toString());
+        /*switch (operador){
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+        }*/
+    }
+
+    public void clickSomaBtn(View view){
+        operacaoText.setText(operacaoText.getText() + " + ");
+        operador = 1;
+    }
+
+    public void clickSubtracaoBtn(View view){
+        operacaoText.setText(operacaoText.getText() + " - ");
+        operador = 2;
+    }
+
+    public void clickDivisaoBtn(View view){
+        operacaoText.setText(operacaoText.getText() + " / ");
+        operador = 3;
+    }
+
+    public void clickMultiplicaoBtn(View view){
+        operacaoText.setText(operacaoText.getText() + " * ");
+        operador = 4;
+    }
+
+
+    public void click0btn(View view) {
+        operacaoText.append("0");
+        //verificaNumero("0");
+    }
+
+    public void click1btn(View view) {
+        operacaoText.append("1");
+        //verificaNumero("1");
+    }
+
+    public void click2btn(View view) {
+        operacaoText.append("2");
+        //verificaNumero("2");
+    }
+
+    public void click3btn(View view) {
+        operacaoText.append("3");
+        //verificaNumero("3");
+    }
+
+    public void click4btn(View view) {
+        operacaoText.append("4");
+        //verificaNumero("4");
+    }
+
+    public void click5btn(View view) {
+        operacaoText.append("5");
+        //verificaNumero("5");
+    }
+
+    public void click6btn(View view) {
+        operacaoText.append("6");
+       // verificaNumero("6");
+    }
+
+    public void click7btn(View view) {
+        operacaoText.append("7");
+       // verificaNumero("7");
+    }
+
+    public void click8btn(View view) {
+        operacaoText.append("8");
+        //verificaNumero("8");
+    }
+
+    public void click9btn(View view) {
+        operacaoText.append("9");
+        //verificaNumero("9");
+    }
+}
---
+++ app/src/main/java/com/HoW/calculadora3/operacoes.java
@@ -0,0 +1,35 @@
+package com.HoW.calculadora3;
+
+
+public class operacoes {
+
+    public static String soma(String a, String b)
+    {
+        String numero1String = "2";//testes
+        String numero2String = "1";
+
+        double numero1 = Double.parseDouble(numero1String);
+        double numero2 = Double.parseDouble(numero2String);
+        double resultado = numero1 + numero2;
+
+        return String.valueOf(resultado);
+    }
+
+
+    public static String subtracao()
+    {
+        return "";
+    }
+
+
+    public static String multiplicacao()
+    {
+        return "";
+    }
+
+
+    public static String divisao()
+    {
+        return "";
+    }
+}
