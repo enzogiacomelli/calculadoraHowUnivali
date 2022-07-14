ccc1517e68c80e20e62bf09b530ef2c8dffddbd5
Enzo Giacomelli <egiacomelli09@gmail.com>
2022-07-13 21:10:41 -0300

    salvar dados na lista
    salva dados em memoria

--- app/src/main/java/com/HoW/calculadora3/HistoricoActivity.java
+++ app/src/main/java/com/HoW/calculadora3/HistoricoActivity.java
@@ -1,14 +1,45 @@
 package com.HoW.calculadora3;
 
+import android.os.Bundle;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
+
 
 public class HistoricoActivity extends AppCompatActivity {
+
+
+    EditText historico;
+    MainActivity main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historico);
+
+        main = new MainActivity();
+        vincularCampo();
+
+
+        listarDadosHistorico();
+    }
+
+    private void vincularCampo()
+    {
+        historico = findViewById(R.id.listagemHistorico);
+    }
+
+    private void listarDadosHistorico ()
+    {
+
+        if (main.saveData.isEmpty()) {
+            historico.setText("Vazio");
+        }else{
+            for (int i=0;i < main.saveData.size();i++) {
+                String a = main.saveData.get(i);
+                historico.setText(historico.getText() + "\n\r" + a);
+            }
+        }
     }
 }
