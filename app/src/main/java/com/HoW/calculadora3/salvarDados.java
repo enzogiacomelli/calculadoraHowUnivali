9a0458c5bfd5188eae67a41580552146a171ff72
Enzo Giacomelli <egiacomelli09@gmail.com>
2022-07-16 14:57:13 -0300

    Update salvarDados.java

--- app/src/main/java/com/HoW/calculadora3/salvarDados.java
+++ app/src/main/java/com/HoW/calculadora3/salvarDados.java
@@ -2,78 +2,56 @@ package com.HoW.calculadora3;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 
 public class salvarDados extends Activity {
-    private int EXTERNAL_STORAGE_PERMISSION_CODE = 23;
-    String aa = "aa";
+    String escrita = "aa";
+    String leitura;
+    //File file = new File("file");
+
 
     public void save() {
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_CODE);
-
-        File file = new File("C:/Users/egiac/AndroidStudioProjects/calculadora3/dados/historico.txt");
-        FileOutputStream fileOutputStream = null;
+        FileOutputStream fos;
         try {
-            fileOutputStream = new FileOutputStream(file);
-            fileOutputStream.write(aa.getBytes());
-            Toast.makeText(this, "Done" + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+            fos = this.openFileOutput("file", MODE_APPEND); //abre ou cria arquivo com nome
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.write(escrita.getBytes());
+            oos.close();
+
+            Toast.makeText(getBaseContext(), "arquivo salvo", Toast.LENGTH_SHORT).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void read(){
+        FileInputStream fis;
+        try {
+            fis = this.openFileInput("C:/Users/egiac/AndroidStudioProjects/calculadora3/dados/historico.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            leitura = ois.readUTF();
+            ois.close();
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (fileOutputStream != null) {
-                try {
-                    fileOutputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-
         }
-
-
-        /*@Override
-        public void onPointerCaptureChanged ( boolean hasCapture){
-            super.onPointerCaptureChanged(hasCapture);
-        }*/
     }
 }
 
-
-
-
-
-   /* public static void criarArquivo() {
-
-            File myObj = new File("C:\\Users\\egiac\\AndroidStudioProjects\\calculadora3\\dados\\historico.txt");
-        try {
-            myObj.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public static void editarArquivo() {
-        try {
-            FileWriter myWriter = new FileWriter("historico.txt");
-            myWriter.write("Files in Java might be tricky, but it is fun enough!");
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }*/
 
 
